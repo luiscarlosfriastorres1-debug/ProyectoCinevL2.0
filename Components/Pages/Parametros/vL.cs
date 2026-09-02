417ac8c4fc064dfc57179c9994d76dd0e7645e9f
Luisca <luiscarlosfriastorres1@gmail.com>
2026-09-02 12:53:42 -0500

    Primer commit

---
+++ Components/Pages/Parametros/vL.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorApp2.Components.Pages.Parametros
+{
+    public class vL
+    {
+        public vL(int id, string titulo, string categoria, string imgurl)
+        {
+            Id = id;
+            Titulo = titulo;
+            Categoria = categoria;
+            imgURL = imgurl;
+        }
+
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Categoria { get; set; }
+
+        public string imgURL { get; set; }
+
+
+
+    }
+}
---
+++ Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp2.Models
+{
+    public class Pelicula
+    {
+
+        public string Titulo { get; set; } = null!;
+
+        public string Genero { get; set; } = null!;
+
+        public int Anio { get; set; }
+
+        public string? ImagenUrl { get; set; }
+
+    }
+}
